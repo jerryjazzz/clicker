e40127c90631d5db3c5fbf1ece7d462c040fd9c6
Chang Chin Sem <chin.sem.chang@gmail.com>
2015-11-26 22:23:10 +0800

    Clean up and bug fix

--- www/js/app.js
+++ www/js/app.js
@@ -14,3 +14,35 @@ angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.services',
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('login', {
+      url: '/login',
+      templateUrl: 'templates/login.html',
+      controller: 'loginController'
+    })
+    .state('signup', {
+      url: '/signup',
+      templateUrl: 'templates/signup.html',
+      controller: 'signupController'
+    })
+    .state('grouplist', {
+      url: '/grouplist',
+      templateUrl: 'templates/grouplist.html',
+      controller: 'groupListController'
+    })
+    .state('group', {
+        url: '/grouplist/:grp_key',
+        templateUrl: 'templates/group.html',
+        controller: 'groupController',
+        params: {
+          grp_key: null
+        }   
+    });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/login');
+
+});
